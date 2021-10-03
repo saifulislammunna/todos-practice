710a4367dcacdf8ee1614a0fe83bbef8c2fa453b
saifulislammunna <saifulmunna28@gmail.com>
2021-10-03 19:03:49 +0600

    todo-detail-added

--- src/components/TodoDetail/TodoDetail.js
+++ src/components/TodoDetail/TodoDetail.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const TodoDetail = () => {
+    const { id} = useParams();
+    const [todo,setTodo] = useState([]);
+    // console.log(params);
+    useEffect(()=>{
+        const url = `https://jsonplaceholder.typicode.com/todos/${id}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setTodo(data));
+    })
     return (
         <div>
-            <h3>Detail coming soon</h3>
+            <h3>Todo Detail of: {id}</h3>
+            <h1>{todo.useId} </h1>
+            <h2>{todo.title}</h2>
+            <h3>{todo.completed?"true":"false"}</h3>
         </div>
     );
 };
